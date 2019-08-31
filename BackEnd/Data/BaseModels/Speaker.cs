46c9aac15cd880be6a8b4e95f9afb2db69318c50
CTheCheese <colby.hunter42@gmail.com>
2019-08-31 03:38:27 -0500

    Initial COmmit

---
+++ BackEnd/Data/BaseModels/Speaker.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Design;
+using BackEnd.Data;
+
+namespace BackEnd.Data
+{
+    public class Speaker : ConferenceDTO.Speaker
+    {
+        public virtual ICollection<SessionSpeaker> SessionSpeakers { get; set; } = new List<SessionSpeaker>();
+    }
+}
